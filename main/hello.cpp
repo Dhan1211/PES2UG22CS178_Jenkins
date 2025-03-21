164890af01f7062dd4c6237e77cb59aeba2c54d3
Dhanya Arun <dhanyaarun1211@gmail.com>
2025-03-21 02:54:23 +0000

    commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
 
-  cout << "Hello!! PES2UG22CS178"
+  cout << "Hello!! PES2UG22CS178";
   return 0;
 }
